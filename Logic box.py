ae854baf1ced063ffd4b8fb23b2c182c5b1dbab5
archanakushvaha <archanakushvaha735@gmail.com>
2026-07-16 13:31:57 +0530

    Update Logic box.py
    Refactor user prompts and output formatting for clarity.

--- Logic box.py
+++ Logic box.py
@@ -1,49 +1,38 @@
 print("Welcome to the Pattern Generator and Number Analyzer!")
 
 while True:
-    print("\nSelect an option:")
-    print("1.Generate a Pattern")
+    print("\n")
+    print("select an option:")
+    print("1.Generate a pattern")
     print("2.Analyze a Range of Numbers")
     print("3.Exit")
-
-    choice=int(input("Enter your Choice:"))
+    print("\n")
+    choice=(int(input("enter your choice:")))
 
     if choice==1:
-
-        rows=int(input("Enter the number of rows for the pattern:"))
-
-        print("\n Pattern")
-
-        for i in range(1,rows+1):
+        row=(int(input("Enter the number of rows for the pattern:")))
+        
+        print("\n")
+        print("pattern:")
+        for i in range(1,row+1):
             for j in range(i):
                 print("*",end="")
             print()
-
     elif choice==2:
-
-        start=int(input("\nEnter the start of the range:"))
-        end=int(input("Enter the end of the range:"))
-
         total=0
-        
-        for i in range(start,end+1):
-            if i%2==0:
-                print(f"Number {i} is Even")
+        start=(int(input("Enter the start of the range:")))
+        end=(int(input("enter the end of the range:")))
+        for num in range(start,end+1):
+            if num%2==0:
+                print(f"number {num} is even")
             else:
-                print(f"Number {i} is Odd")
-
-            total +=i
-                
-        print(f"Sum of all numbers from {start} to {end} is:{total}")
-
-    elif choice==3:
-        print("Exiting the program.Goodbye!")
-
-        break
+                print(f"number {num}is odd")
+            total+=num
+        print(f"sum of all numbers from {start} to {end} is:{total}")
     
+    elif choice==3:
+        print("Exiting the program ! good bye")
+        break
     else:
-        print("Invalide choice")
-        
-
-
+        print("invalid choice!")
 
